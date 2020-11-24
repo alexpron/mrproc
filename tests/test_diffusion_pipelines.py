44cd187e6744e153395c38661ae5c9393ee029ab
pron.a <alexandre.pron@gmaiL.com>
2020-11-24 10:28:43 +0100

    UPDATE: added scripts for manual processing of tractogram and extraction of maxima from a volume

--- tests/test_diffusion_pipelines.py
+++ tests/test_diffusion_pipelines.py
@@ -20,7 +20,7 @@ def test_create_diffusion_pipeline():
     inputnode.inputs.bvecs = '/hpc/banco/Primavoice_Data_and_Analysis/DTI/cerimed/sub-04/dmri/default_acquisition/default_analysis/corrected_bvecs_sub-04.txt'
     inputnode.inputs.t1_volume = '/hpc/banco/Primavoice_Data_and_Analysis/analysis_sub-04/anat/sub-04_ses-01_T1w_denoised_debiased_in-MNI152.nii.gz'
     diffusion_pipeline.write_graph(graph2use="colored", format="png")
-    diffusion_pipeline.run()
+    #diffusion_pipeline.run()
 
 
 if __name__ == "__main__":
